75189da9b39e4fc9b283fc4ee954d26e2cecef15
Nikolas Kuhlen <nikolas.kuhlen@uni-bonn.de>
2016-01-27 22:59:47 +0100

    Added .m-file to replicate figure 1.

---
+++ src/final/Replication_Figure_1_updated.m
@@ -0,0 +1,72 @@
+clear all
+close all
+
+%create variables
+timeline = xlsread(project_paths('IN_DATA', 'DataSet_updated.xlsx'),'FFA', 'L9:L262');
+equity = xlsread(project_paths('IN_DATA', 'DataSet_updated.xlsx'),'FFA', 'I9:I262');
+debt = xlsread(project_paths('IN_DATA', 'DataSet_updated.xlsx'),'FFA', 'J9:J262');
+
+
+
+%create figure
+figure
+
+%scale variables to match figure from paper
+d = debt*100;
+e = equity*100;
+
+%add recession areas
+load(project_paths('IN_DATA', 'recessiondates.mat'))
+
+%add reference line at y=0
+hold on
+hline = refline (0);
+hline.Color = 'k';
+hold off
+
+% use code from plot_NBER_recessions
+plothandle = plot(timeline, d, 'b', timeline, e, 'r--');
+
+%adjust axes
+axis([1952 2015 -16 16])
+aaa=ylim;
+bottom=aaa(1,1);
+top=aaa(1,2);
+
+%delete dates that happen before first data point
+x_dates=xlim;
+recessiondates(recessiondates(:,2)<x_dates(1),:)=[];
+
+%set axes limits and step size
+set(gca, 'XTick', [1956:4:2015])
+set(gca, 'YTick', [-16:4:16])
+
+%add recession areas
+for ii=1:length(recessiondates)
+hold on
+ha = area([recessiondates(ii,1) recessiondates(ii,2)], [bottom top-bottom; bottom top-bottom],'FaceColor',[0 0.7 0.8],'EdgeColor','white');
+set(ha(1), 'FaceColor', 'none') % this makes the bottom area invisible
+set(ha, 'LineStyle', '-')
+end
+ylim(aaa)
+
+legend('Debt repurchase', 'Equity payout', 'Location','northwest')
+uistack(plothandle,'top')
+set(gca,'Layer','top')
+
+%this exports a graphic without white space
+ti = get(gca,'TightInset')
+set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
+
+set(gca,'units','centimeters')
+pos = get(gca,'Position');
+ti = get(gca,'TightInset');
+
+set(gcf, 'PaperUnits','centimeters');
+set(gcf, 'PaperSize', [pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
+set(gcf, 'PaperPositionMode', 'manual');
+set(gcf, 'PaperPosition',[0 0 pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
+
+% set(f,'PaperPositionMode','auto');
+% saveas(figure, project_paths('OUT_FIGURES', 'Replication_Figure_1_updated.png'));
+saveas(gcf, project_paths('OUT_FIGURES', 'Replication_Figure_1_updated.pdf'));
