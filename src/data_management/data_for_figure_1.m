c85ced8e5872f6878dc61046e21ce16db4f6c82e
Nikolas Kuhlen <nikolas.kuhlen@uni-bonn.de>
2016-01-28 15:47:12 +0100

    Use correct file suffix and added saving command.

--- src/data_management/data_for_figure_1.m
+++ src/data_management/data_for_figure_1.m
@@ -4,6 +4,7 @@
 %% Import original data
 data_ffa = xlsread(project_paths('IN_DATA', 'FRB_Z1.xlsx'), 'Sheet1', 'B8:F261');
 
+net_increase_credit_markets_instruments = data_ffa(:,1);
 net_increase_corporate_equities         = data_ffa(:,2);
 net_dividends_non_financial_business    = data_ffa(:,3);
 net_dividends_farm_business             = data_ffa(:,4);
@@ -15,3 +16,6 @@ equity_payout = net_dividends_non_financial_business ...
                 - net_increase_corporate_equities ...
                 - proprietors_net_investment
 
+save(project_paths('OUT_DATA', 'data_figure_1.mat'), 'net_increase_credit_markets_instruments', ...
+     'net_increase_corporate_equities', 'net_dividends_non_financial_business', ...
+     'net_dividends_farm_business', 'proprietors_net_investment')
