6c627e34f1ba40fbda3c17a1b0b7a53bb046d598
Nikolas Kuhlen <nikolas.kuhlen@uni-bonn.de>
2016-01-28 17:52:04 +0100

    Added correct calculation of equity payout.

--- src/data_management/data_for_figure_1.m
+++ src/data_management/data_for_figure_1.m
@@ -4,8 +4,8 @@
 
 %% Import original data
 data_ffa 							= xlsread(project_paths('IN_DATA', 'FRB_Z1.xlsx'), 'Sheet1', 'B8:F261');
-business_value_added_until_1969 	= xlsread(project_paths('IN_DATA', 'NIPA_Hist_until_1969.xlsx'), '10305 Qtr', 'D11:CQ11');
-business_value_added_from_1970 		= xlsread(project_paths('IN_DATA', 'NIPA_Hist_from_1969.xlsx'), '10305 Qtr', 'H11:GH11');
+business_value_added_until_1969 	= xlsread(project_paths('IN_DATA', 'NIPA_Hist_until_1969.xlsx'), '10305 Qtr', 'X11:CQ11');
+business_value_added_from_1970 		= xlsread(project_paths('IN_DATA', 'NIPA_Hist_from_1969.xlsx'), '10305 Qtr', 'H11:GG11');
 
 % Define variables
 net_increase_credit_markets_instruments = data_ffa(:,1);
@@ -16,11 +16,11 @@ proprietors_net_investment              = data_ffa(:,5);
 business_gdp 							= transpose(horzcat(business_value_added_until_1969, business_value_added_from_1970))
 
 % Calculate equity payout
-equity_payout = net_dividends_non_financial_business ...
+equity_payout = (net_dividends_non_financial_business ...
                 + net_dividends_farm_business ...
                 - net_increase_corporate_equities ...
-                - proprietors_net_investment
+                - proprietors_net_investment)./(business_gdp*1000)
 
 save(project_paths('OUT_DATA', 'data_figure_1.mat'), 'net_increase_credit_markets_instruments', ...
      'net_increase_corporate_equities', 'net_dividends_non_financial_business', ...
-     'net_dividends_farm_business', 'proprietors_net_investment')
+     'net_dividends_farm_business', 'proprietors_net_investment', 'business_gdp', 'equity_payout')
