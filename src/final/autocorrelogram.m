96831a1b1ca617f79a73254fd813b5baffeb1928
Nikolas Kuhlen <nikolas.kuhlen@uni-bonn.de>
2016-01-31 23:49:55 +0100

    Corrected path of mat file.

--- src/final/autocorrelogram.m
+++ src/final/autocorrelogram.m
@@ -5,7 +5,7 @@ Create graphs of autocorrelation in residual series.
 clear all;
 close all;
 
-load(project_paths('OUT_DATA', 'series_of_shocks.mat'));
+load(project_paths('OUT_ANALYSIS', 'series_of_shocks.mat'));
 
 autocorr(r_tfp, 15);
 saveas(gcf, project_paths('OUT_FIGURES', 'autocorrelation_tfp.png'));
