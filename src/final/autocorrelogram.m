0fb994d5f0ced658236e0f1daa8cba3ee68b8427
Nikolas Kuhlen <nikolas.kuhlen@uni-bonn.de>
2016-01-31 20:52:07 +0100

    Added file to create autocorrelograms.

---
+++ src/final/autocorrelogram.m
@@ -0,0 +1,14 @@
+%{ 
+Create graphs of autocorrelation in residual series.
+%}
+
+clear all;
+close all;
+
+load(project_paths('OUT_DATA', 'series_of_shocks.mat'));
+
+autocorr(r_tfp, 15);
+saveas(gcf, project_paths('OUT_FIGURES', 'autocorrelation_tfp.png'));
+
+autocorr(r_xi, 15);
+saveas(gcf, project_paths('OUT_FIGURES', 'autocorrelation_xi.png'));
