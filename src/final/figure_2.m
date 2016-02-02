1194e8b774d6e71ebcb02d9ab46870a10c821788
Nikolas Kuhlen <nikolas.kuhlen@uni-bonn.de>
2016-02-02 19:13:12 +0100

    Added semicolons.

--- src/final/figure_2.m
+++ src/final/figure_2.m
@@ -14,68 +14,68 @@ load(project_paths('OUT_ANALYSIS', 'series_of_shocks.mat'));
 
 % Create the plot in position 1 of a 2x2 grid
 NumTicks =11;
-FontSizeTitle = 12
-FontSizeAxis = 8
+FontSizeTitle = 12;
+FontSizeAxis = 8;
 
 figure
-subaxis(2,2,1, 'Spacing', 0.08)
-plot(TFPSeqa*100)
-axis tight
-set(gca,'box','off')
-set(gca,'XLim',[1 120])
-set(gca,'YLim',[-8 8])
+subaxis(2,2,1, 'Spacing', 0.08);
+plot(TFPSeqa*100);
+axis tight;
+set(gca,'box','off');
+set(gca,'XLim',[1 120]);
+set(gca,'YLim',[-8 8]);
 L = get(gca,'XLim');
-set(gca,'XTick',linspace(L(1),L(2),NumTicks))
+set(gca,'XTick',linspace(L(1),L(2),NumTicks));
 set(gca,'XMinorTick','on');
-title('Level of productivity, z','Interpreter','latex', 'FontSize', FontSizeTitle, 'FontSize', FontSizeTitle)
-set(gca,'FontSize',FontSizeAxis)
+title('Level of productivity, z','Interpreter','latex', 'FontSize', FontSizeTitle, 'FontSize', FontSizeTitle);
+set(gca,'FontSize',FontSizeAxis);
 set(gca,'XTickLabel',['85(II)'; '88(II)'; '91(II)'; '94(II)'; '97(II)'; ...
-	'00(II)'; '03(II)'; '06(II)'; '09(II)';'12(II)'; '15(II)'])
+	'00(II)'; '03(II)'; '06(II)'; '09(II)';'12(II)'; '15(II)']);
 
 % Create the bar chart in position 2 of a 2x2 grid
 subaxis(2,2,2, 'Spacing', 0.08)
 plot(xiSeqa*100)
-axis tight
-set(gca,'box','off')
-set(gca,'XLim',[1 120])
-set(gca,'YLim',[-8 8])
+axis tight;
+set(gca,'box','off');
+set(gca,'XLim',[1 120]);
+set(gca,'YLim',[-8 8]);
 L = get(gca,'XLim');
-set(gca,'XTick',linspace(L(1),L(2),NumTicks))
+set(gca,'XTick',linspace(L(1),L(2),NumTicks));
 set(gca,'XMinorTick','on');
-title('Level of financial conditions, $\xi$','Interpreter','latex', 'FontSize', FontSizeTitle)
-set(gca,'FontSize',FontSizeAxis)
+title('Level of financial conditions, $\xi$','Interpreter','latex', 'FontSize', FontSizeTitle);
+set(gca,'FontSize',FontSizeAxis);
 set(gca,'XTickLabel',['85(II)'; '88(II)'; '91(II)'; '94(II)'; '97(II)'; ...
-	'00(II)'; '03(II)'; '06(II)'; '09(II)';'12(II)'; '15(II)'])
+	'00(II)'; '03(II)'; '06(II)'; '09(II)';'12(II)'; '15(II)']);
 
 % Create the stem chart in position 3 of a 2x2 grid
 subaxis(2,2,3, 'Spacing', 0.08)
 plot(ResidualTFP*100)
-axis tight
-set(gca,'box','off')
-set(gca,'XLim',[1 120])
-set(gca,'YLim',[-3 3])
+axis tight;
+set(gca,'box','off');
+set(gca,'XLim',[1 120]);
+set(gca,'YLim',[-3 3]);
 L = get(gca,'XLim');
-set(gca,'XTick',linspace(L(1),L(2),NumTicks))
+set(gca,'XTick',linspace(L(1),L(2),NumTicks));
 set(gca,'XMinorTick','on');
-title('Innovations to productivity,  ${\epsilon}_{z}$','Interpreter','latex', 'FontSize', FontSizeTitle)
-set(gca,'FontSize',FontSizeAxis)
+title('Innovations to productivity,  ${\epsilon}_{z}$','Interpreter','latex', 'FontSize', FontSizeTitle);
+set(gca,'FontSize',FontSizeAxis);
 set(gca,'XTickLabel',['85(II)'; '88(II)'; '91(II)'; '94(II)'; '97(II)'; ...
-	'00(II)'; '03(II)'; '06(II)'; '09(II)';'12(II)'; '15(II)'])
+	'00(II)'; '03(II)'; '06(II)'; '09(II)';'12(II)'; '15(II)']);
 
 % Create the line plot in position 4 of a 2x2 grid
 subaxis(2,2,4, 'Spacing', 0.08)
 plot(ResidualXi*100)
-axis tight
-set(gca,'box','off')
-set(gca,'XLim',[1 120])
-set(gca,'YLim',[-3 3])
+axis tight;
+set(gca,'box','off');
+set(gca,'XLim',[1 120]);
+set(gca,'YLim',[-3 3]);
 L = get(gca,'XLim');
-set(gca,'XTick',linspace(L(1),L(2),NumTicks))
+set(gca,'XTick',linspace(L(1),L(2),NumTicks));
 set(gca,'XMinorTick','on');
-title('Innovations to financial conditions,  ${\epsilon}_{\xi}$','Interpreter','latex', 'FontSize', FontSizeTitle)
-set(gca,'FontSize',FontSizeAxis)
+title('Innovations to financial conditions,  ${\epsilon}_{\xi}$','Interpreter','latex', 'FontSize', FontSizeTitle);
+set(gca,'FontSize',FontSizeAxis);
 set(gca,'XTickLabel',['85(II)'; '88(II)'; '91(II)'; '94(II)'; '97(II)'; ...
-	'00(II)'; '03(II)'; '06(II)'; '09(II)';'12(II)'; '15(II)'])
+	'00(II)'; '03(II)'; '06(II)'; '09(II)';'12(II)'; '15(II)']);
 
 
 saveas(gcf, project_paths('OUT_FIGURES', 'figure_2.png'));
