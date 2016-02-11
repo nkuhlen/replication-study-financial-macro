abbad1dff2904c9d6e80fc5651acd399e1a1cd7c
Nikolas Kuhlen <nikolas.kuhlen@uni-bonn.de>
2016-02-11 19:19:00 +0100

    Make autocorrelograms invisible when running script.

--- src/final/autocorrelogram.m
+++ src/final/autocorrelogram.m
@@ -7,8 +7,12 @@ close all;
 
 load(project_paths('OUT_ANALYSIS', 'series_of_shocks.mat'));
 
+figure;
+set(gcf, 'visible', 'off');
 autocorr(ResidualTFP, 15);
 saveas(gcf, project_paths('OUT_FIGURES', 'autocorrelation_tfp.png'));
 
+figure;
+set(gcf, 'visible', 'off');
 autocorr(ResidualXi, 15);
 saveas(gcf, project_paths('OUT_FIGURES', 'autocorrelation_xi.png'));
