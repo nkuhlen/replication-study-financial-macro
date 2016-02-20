488b4937ef685d79e725d7353410863b0d03a198
Nikolas Kuhlen <nikolas.kuhlen@uni-bonn.de>
2016-02-20 15:13:36 +0100

    Added index correction to loops.

--- src/final/replication_table_3.m
+++ src/final/replication_table_3.m
@@ -48,15 +48,7 @@ fclose(fid);
 
 % Create list of names for estimated parameters in the order they shall
 % appear in the final table.
-est_par = {'eps_z',
-           'eps_zeta',
-           'eps_gamma',
-           'eps_eta',
-           'eps_upsilon',
-           'eps_G',
-           'eps_varsigma',
-           'eps_xi',
-           'siggma',
+est_par = {'siggma',
            'epsilon',
            'h',
            'omega',
@@ -75,6 +67,14 @@ est_par = {'eps_z',
            'rho_varsigma',
            'rho_xi',
            'rho_gz',
+           'eps_z',
+           'eps_zeta',
+           'eps_gamma',
+           'eps_eta',
+           'eps_upsilon',
+           'eps_G',
+           'eps_varsigma',
+           'eps_xi',
            'rho_R',
            'nu_1',
            'nu_2',
@@ -86,33 +86,46 @@ str_distr = {'Beta', 'Normal', 'IGamma'};
 distrib = [1, 3, 4];
 dist_dict = containers.Map(distrib, str_distr);
 
+% Create a list of indices of the elements that are strings corresponding to 
+% shocks.
+index_shocks = strfind(est_par, 'eps_');
+shocks = find(not(cellfun('isempty', index_shocks)));
+
 fid = fopen(path_out_table_est, 'w');
 
 for idx = 1:length(est_par)
 
+    shocks_looped_over = shocks <= idx;
+    shocks_counter = numel(shocks_looped_over(shocks_looped_over>0));
+
     param = est_par(idx,:);
+
     % Note that information for the estimation of the shocks are stored
     % differently from the information of estimated parameters. Hence we
     % first check for the estimated shocks.
-    if idx <= size(estim_params_.var_exo,1)
+    if ismember(idx, shocks)
+        index_correction = idx -  shocks_counter;
+
         long_name = M_.exo_names_long(strmatch(param, M_.exo_names, ...
                                                'exact'),:);
         tex_name = M_.exo_names_tex(strmatch(param, M_.exo_names, 'exact'),: ...
                                     );
-        par_dist = dist_dict(estim_params_.var_exo(idx, 5));
-        par_mean = estim_params_.var_exo(idx, 6);
-        par_std = estim_params_.var_exo(idx, 7);
+        par_dist = dist_dict(estim_params_.var_exo(idx - index_correction, 5));
+        par_mean = estim_params_.var_exo(idx - index_correction, 6);
+        par_std = estim_params_.var_exo(idx - index_correction, 7);
         post_mode = oo_.posterior_mode.shocks_std.(char(param));
         post_hpdinf = oo_.posterior_hpdinf.shocks_std.(char(param));
         post_hpdsup = oo_.posterior_hpdsup.shocks_std.(char(param));
     else
+        index_correction = shocks_counter;
+
         long_name = M_.param_names_long(strmatch(param, M_.param_names, ...
                                                'exact'),:);
         tex_name = M_.param_names_tex(strmatch(param, M_.param_names, ...
                                                'exact'),:);
-        par_dist = dist_dict(estim_params_.param_vals(idx - 8, 5));
-        par_mean = estim_params_.param_vals(idx - 8, 6);
-        par_std = estim_params_.param_vals(idx - 8, 7);
+        par_dist = dist_dict(estim_params_.param_vals(idx - index_correction, 5));
+        par_mean = estim_params_.param_vals(idx - index_correction, 6);
+        par_std = estim_params_.param_vals(idx - index_correction, 7);
         post_mode = oo_.posterior_mode.parameters.(char(param));
         post_hpdinf = oo_.posterior_hpdinf.parameters.(char(param));
         post_hpdsup = oo_.posterior_hpdsup.parameters.(char(param));
