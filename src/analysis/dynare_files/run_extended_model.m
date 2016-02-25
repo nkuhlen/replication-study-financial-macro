16d2437ebc1d1e305169bc5c6dceed21d18125c4
Nikolas Kuhlen <nikolas.kuhlen@uni-bonn.de>
2016-02-25 09:56:56 +0100

    Added traceplots and correlograms for posterior draws.

--- src/analysis/dynare_files/run_extended_model.m
+++ src/analysis/dynare_files/run_extended_model.m
@@ -1,6 +1,4 @@
-%{
-Run the extended model dynare file.
-%}
+% Run the extended model
 
 %% Path settings
 
@@ -19,15 +17,40 @@ copyfile(path_to_data, 'estimation_data.mat');
 %% Run dynare file
 dynare extended_model.mod
 
+% Create Trace plots and autocorrelogram for convergence diagnostics
+% options_.trace_plot_ma = 300000;
+trace_plot(options_,M_,estim_params_,'StructuralShock',1,'eps_z');
+trace_plot(options_,M_,estim_params_,'StructuralShock',1,'eps_xi');
+
+mh_autocorrelation_function(options_,M_,estim_params_, 'StructuralShock',1, ...
+                            'eps_z');
+mh_autocorrelation_function(options_,M_,estim_params_, 'StructuralShock',1, ...
+                            'eps_xi');
+
 %% Move desired output
 
 % The output path settings have to be defined after the dynare command,
 % otherwise matlab will throw an error at this point.
 output_path = project_paths('OUT_ANALYSIS');
+dynare_figure = project_paths('DYNARE_FILES', 'extended_model/graphs/');
+figure_output = project_paths('OUT_FIGURES');
+
 % output_path = '../../../bld/out/analysis/';
+% dynare_figure = 'extended_model/graphs/';
+% figure_output = '../../../bld/out/figures/';
 
 if ~exist(output_path, 'dir')
     mkdir(output_path)
 end
 
+if ~exist(figure_output, 'dir')
+    mkdir(figure_output)
+end
+
 copyfile('extended_model_results.mat', output_path)
+copyfile(strcat(dynare_figure, 'TracePlot_SE_eps_xi.eps'), figure_output)
+copyfile(strcat(dynare_figure, 'TracePlot_SE_eps_z.eps'), figure_output)
+copyfile(strcat(dynare_figure, 'MH_Autocorrelation_SE_eps_xi.eps'), ...
+         figure_output)
+copyfile(strcat(dynare_figure, 'MH_Autocorrelation_SE_eps_z.eps'), ...
+         figure_output)
