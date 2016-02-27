e59445b9d51f6dc882fd3537a114b145ac1494a6
Nikolas Kuhlen <nikolas.kuhlen@uni-bonn.de>
2016-02-27 22:15:47 +0100

    Use variable names as defined by Benjamin Born.

--- src/data_management/simulation_data_preparation.m
+++ src/data_management/simulation_data_preparation.m
@@ -45,15 +45,15 @@ pi_obs = inflation - mean(inflation);
 
 % Assuming that there is no trend in the federal funds rate, I just demean
 % the series.
-r_obs = fed_funds(2:end) - nanmean(fed_funds(2:end)); 
+r_obs = fed_funds(2:end) - mean(fed_funds(2:end)); 
 
 hours_growth = log(hours(2:end)) - log(hours(1:end-1));
-n_obs = hours_growth - nanmean(hours_growth);
+n_obs = hours_growth - mean(hours_growth);
 
 % Note that wage data are already quoted in real terms. So no normalizing by
 % prices is required here.
 wage_growth = log(wages(2:end)) - log(wages(1:end-1)); 
-W_obs = wage_growth - nanmean(wage_growth);
+W_obs = wage_growth - mean(wage_growth);
 
 % Here I use the same procedure to fit the observed data to the model that
 % was used for the comparison of the simple model with the data.
@@ -63,58 +63,3 @@ debt_repurchase_obs = detrend(debt_rep(2:end));
 % estimating the variables
 save(path_output_data, 'y_obs', 'c_obs', 'invest_obs', 'pi_obs', 'r_obs', 'n_obs', ...
      'W_obs', 'debt_repurchase_obs')
-
-% % Plot all outcome variables to see whether the results look reasonable,
-% % i.e. that there is no clear trend and they are centred around zero.
-% figure
-% % set(gcf, 'visible', 'off')
-%     subplot(4, 2, 1)
-%     plot(timeline(2:end), y_obs)
-%     xlim([1984, 2010.25])
-%     ylabel('y_obs')
-%     title('GDP')
-    
-    
-%     subplot(4, 2, 2)
-%     plot(timeline(2:end), c_obs)
-%     xlim([1984, 2010.25])
-%     ylabel('c_obs')
-%     title('Consumption')
-    
-%     subplot(4, 2, 3)
-%     plot(timeline(2:end), invest_obs)
-%     xlim([1984, 2010.25])
-%     ylabel('invest_obs')
-%     title('Investment')
-    
-%     subplot(4, 2, 4)
-%     plot(timeline(2:end), pi_obs)
-%     xlim([1984, 2010.25])
-%     ylabel('pi_obs')
-%     title('Inflation')
-    
-%     subplot(4, 2, 5)
-%     plot(timeline(2:end), r_obs)
-%     xlim([1984, 2010.25])
-%     ylabel('r_obs')
-%     title('Federal Funds Rate')
-    
-%     subplot(4, 2, 6)
-%     plot(timeline(2:end), n_obs)
-%     xlim([1984, 2010.25])
-%     ylabel('n_obs')
-%     title('Working hours')
-    
-%     subplot(4, 2, 7)
-%     plot(timeline(2:end), W_obs)
-%     xlim([1984, 2010.25])
-%     ylabel('W_obs')
-%     title('Wages')
-    
-%     subplot(4, 2, 8)
-%     plot(timeline(2:end), debt_repurchase_obs)
-%     xlim([1984, 2010.25])
-%     ylabel('debt_repurchase_obs')
-%     title('Debt repurchases')
-    
-% saveas(gcf, 'mean_zero_plots.pdf')
